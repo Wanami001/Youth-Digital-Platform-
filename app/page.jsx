0b80a12dbc29e54876522d8f6257948bc9771dea
Wanami001 <213154857+Wanami001@users.noreply.github.com>
2025-05-24 14:19:10 +0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to the Youth Digital Platform</h1>
+      <p>This is a placeholder homepage. More features coming soon!</p>
+    </main>
+  );
+}
